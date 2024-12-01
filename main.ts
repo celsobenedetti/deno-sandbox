6d99bd93527bda187bedc91682ee9167523d8d6b
Celso Benedetti <celsobenedetti2@gmail.com>
2024-12-01 13:28:19 -0300

    fix: deno check

--- main.ts
+++ main.ts
@@ -1,11 +1,9 @@
-import { toCamelCase } from "jsr:@std/text";
-
-function hello(text: string) {
-  console.log(toCamelCase(text));
+export function add(a: number, b: number) {
+  return a + b;
 }
 
 // Only true if this file is run directly from Deno cli
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  hello("Hello, world!");
+  add(1, 2);
 }
