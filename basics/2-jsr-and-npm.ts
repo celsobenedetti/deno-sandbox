ec41237e659d79f339ecd82b99bdef690724a897
Celso Benedetti <celsobenedetti2@gmail.com>
2024-12-01 13:25:41 -0300

    feat: lesson 2 JSR

---
+++ basics/2-jsr-and-npm.ts
@@ -0,0 +1,16 @@
+import { invert } from "jsr:@std/collections";
+
+const someObject = { a: "x", b: "y", c: "z" };
+
+/**
+ * Challenge: use a tool from @std/collections to invert the keys and values
+ */
+function invertKeysAndValues(obj: Record<string, string>) {
+  return invert(obj);
+}
+
+if (import.meta.main) {
+  console.log(
+    invertKeysAndValues(someObject),
+  );
+}
