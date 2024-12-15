793372d9aa082a859eac6e40e9a4442540b464ed
Celso Benedetti <celsobenedetti2@gmail.com>
2024-12-15 16:16:51 -0300

    init: link-share project

--- main.ts
+++
@@ -1,9 +0,0 @@
-export function add(a: number, b: number) {
-  return a + b;
-}
-
-// Only true if this file is run directly from Deno cli
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  add(1, 2);
-}
